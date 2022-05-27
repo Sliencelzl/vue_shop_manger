e56fc12681425c1da7d3987e575bbe84bd341119
Sliencelzl <lzl1160016315@163.com>
2022-05-27 16:10:14 +0800

    init project

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,48 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <my-header background="red" color title="我的购物车"></my-header>
+    <div class="main">
+      <my-goods v-for="obj in list" :key="obj.id"  :gObj="obj"></my-goods>
+    </div>
+    <my-footer @changeAll="changeFn" :arr="list"></my-footer>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
 
+import MyHeader from './components/myHeader.vue'
+import MyGoods from './components/myGoods.vue'
+import MyFooter from './components/myFooter.vue'
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    MyHeader,MyGoods,MyFooter
+  },
+  data(){
+    return{
+      list:[]
+    }
+  },
+  created(){
+    this.$axios({
+      url:'/api/cart',
+      method:"Get"
+    }).then(res=>{
+      this.list = res.data.list
+      console.log(this.list)
+    })
+  },
+  methods:{
+    changeFn(bool){
+      this.list.forEach(obj=>obj.goods_state = bool)
+    }
   }
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+.main{
+  padding-top: 45px;
+  padding-bottom: 50px;
 }
 </style>
